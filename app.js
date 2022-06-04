39795a5f416a9eebe90a1c9235069a610fa48541
WilsonF78 <fortes.wilson78@gmail.com>
2022-06-04 18:18:14 +0200

    checkVictoire

--- app.js
+++ app.js
@@ -20,82 +20,81 @@ let c8 = document.getElementById("c8")
 let c9 = document.getElementById("c9")
 
 
+
 let cAll = [c1, c2, c3, c4, c5, c6, c7, c8, c9]
 
-let joueurActif = "X"
 let joueurOne = "X"
 let joueurTwo = "O"
-let restartButton = document.getElementById('restartButton')
+let joueurActif = joueurOne
+let win = document.getElementById("win")
 let resetGame = document.getElementById('reset-game');
-let winner = "Win"
 
-
-
-//j'attribue un clique au C1 que j 'ai recuperé
-// c1.addEventListener("click", () => {
-//     console.log("c1 vient d'etre cliquee")
-// })
-
-// pour chq elements du tableau cAll
-//   -> .addEventListener
 
 for (let i = 0; i < cAll.length; i++) {
     cAll[i].addEventListener("click", () =>
     {
-        // tu viens de cliquer sur la case i
-        //
-        // 1. la case est vide => mettre un x ou un o
-        // 2. la case contient un x => rien
-        // 3. la case contient un o => rien
 
-        // if (cAll[i].innerHTML === "") {
-        //     console.log("test")
-        //     (cAll[i].innerHTML = joueurOne)
+        //
+        // mettre alternativement une x/o
+        // change le joueur actif
+        //
 
         if (cAll[i].innerHTML === "") {
             cAll[i].innerHTML = joueurActif
             if (joueurActif == joueurOne) {
                 joueurActif = joueurTwo
-
             } else {
                 joueurActif = joueurOne
-
             }
+
+            checkVictoir()
         }
-
-
-        //     // rajouter un O ou un X
-
-
-        // } else {
-        //     console.log("non vide");
-
-        //     // RIEN
-        // }
-
-
-
-
-        // 1.
-        // si la case est vide => console.log('vide')
-        // sinon, console.log('pas vide')
-
-
-
-
-
-
-        ///////////////////////////////////////////
-        // si i = test ou i = a joueur actif 
-        //retusn 
-
-        // if ([i] == "" || [i] == joueurActif) {
-        //     return
-        // }
-
-
-
     })
 }
 
 
+
+function checkVictoir()
+{
+    if ((joueurOne == c1.innerHTML && c1.innerHTML == c2.innerHTML && c2.innerHTML == c3.innerHTML) ||
+        (joueurOne == c4.innerHTML && c4.innerHTML == c5.innerHTML && c5.innerHTML == c6.innerHTML) ||
+        (joueurOne == c7.innerHTML && c7.innerHTML == c8.innerHTML && c8.innerHTML == c9.innerHTML) ||
+        (joueurOne == c1.innerHTML && c1.innerHTML == c4.innerHTML && c4.innerHTML == c7.innerHTML) ||
+        (joueurOne == c2.innerHTML && c2.innerHTML == c5.innerHTML && c5.innerHTML == c8.innerHTML) ||
+        (joueurOne == c3.innerHTML && c3.innerHTML == c6.innerHTML && c6.innerHTML == c9.innerHTML) ||
+        (joueurOne == c1.innerHTML && c1.innerHTML == c5.innerHTML && c5.innerHTML == c9.innerHTML) ||
+        (joueurOne == c7.innerHTML && c7.innerHTML == c5.innerHTML && c5.innerHTML == c3.innerHTML)) {
+
+        victoireJ1()
+
+    } else if (
+        (joueurTwo == c1.innerHTML && c1.innerHTML == c2.innerHTML && c2.innerHTML == c3.innerHTML) ||
+        (joueurTwo == c4.innerHTML && c4.innerHTML == c5.innerHTML && c5.innerHTML == c6.innerHTML) ||
+        (joueurTwo == c7.innerHTML && c7.innerHTML == c8.innerHTML && c8.innerHTML == c9.innerHTML) ||
+        (joueurTwo == c1.innerHTML && c1.innerHTML == c4.innerHTML && c4.innerHTML == c7.innerHTML) ||
+        (joueurTwo == c2.innerHTML && c2.innerHTML == c5.innerHTML && c5.innerHTML == c8.innerHTML) ||
+        (joueurTwo == c3.innerHTML && c3.innerHTML == c6.innerHTML && c6.innerHTML == c9.innerHTML) ||
+        (joueurTwo == c1.innerHTML && c1.innerHTML == c5.innerHTML && c5.innerHTML == c9.innerHTML) ||
+        (joueurTwo == c7.innerHTML && c7.innerHTML == c5.innerHTML && c5.innerHTML == c3.innerHTML))
+
+        victoireJ2()
+
+}
+
+
+// [1]  [2]  [3]
+
+// [4]  [5]  [6]
+
+// [7]  [8]  [9]
+
+function victoireJ1()
+{
+    window.alert("Victoir du joueur 1");
+}
+
+
+function victoireJ2()
+{
+    window.alert("Victoir du joueur 2");
+}
